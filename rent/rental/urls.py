e29b410006db10104eb5377556af1ace6184daf7
Kemal Eren Sunar <kerensunar@outlook.com>
2024-02-21 15:51:49 +0300

    Added functionality to display user rentals in the 'My Rentals' section

--- rent/rental/urls.py
+++ rent/rental/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('rental/<int:uav_id>/', views.rental_function, name='rental_function'),
+    path('my_rentals/', views.my_rentals, name='my_rentals'),
+    path('cancel_rental/<int:rental_id>/', views.cancel_rental, name='cancel_rental'),
 ]
--- rent/rental/views.py
+++ rent/rental/views.py
@@ -4,6 +4,8 @@ from django.utils import timezone
 from .models import Rental
 from uav_.models import UAV
 from django.contrib import messages
+from datetime import datetime
+from django.utils.timezone import make_aware
 
 @login_required
 def rental_function(request, uav_id):
@@ -16,9 +18,33 @@ def rental_function(request, uav_id):
         uav.save()
         messages.add_message(request, messages.SUCCESS,'Rental completed.')
         return redirect('index')  # Redirect to home page if rental is successful
+    #Disable rent button if UAV is already rented
     # else:
     #     messages.add_message(request, messages.WARNING, "This UAV has already been rented.")
     #     return redirect('index')  # Redirect to home page if UAV is already rented
 
 
+
+@login_required
+def my_rentals(request):
+    rentals = Rental.objects.filter(renter=request.user)
+    for rental in rentals:
+        # Anlık tarihi al
+        current_datetime = timezone.now()
+        # Kiralamanın bitiş tarihini bir datetime nesnesine dönüştür
+        rental_end_datetime = make_aware(datetime.combine(rental.rental_end_date, datetime.min.time()))
+        # Kalan günleri hesapla
+        remaining_days = (rental_end_datetime - current_datetime).days
+        # rental nesnesine kalan günleri ekle
+        rental.remaining_days = remaining_days
+
+    context = {
+        'rentals': rentals
+    }
+    return render(request, 'rentals/my_rentals.html', context)
             
+
+def cancel_rental(request, rental_id):
+    rental = Rental.objects.get(id=rental_id)
+    rental.delete()
+    return redirect('my_rentals')  # veya başka bir URL'ye yönlendirme yapın
